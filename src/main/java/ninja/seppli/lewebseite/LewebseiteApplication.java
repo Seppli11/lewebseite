d6887cb3956fd682f3cabe939a4bdfc1cb351420
Sebastian Zumbrunn <sebastian.zumbrunn@gmail.com>
2020-04-27 10:06:48 +0200

    initial commit

---
+++ src/main/java/ninja/seppli/lewebseite/LewebseiteApplication.java
@@ -0,0 +1,13 @@
+package ninja.seppli.lewebseite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LewebseiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LewebseiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ninja/seppli/lewebseite/LewebseiteApplicationTests.java
@@ -0,0 +1,13 @@
+package ninja.seppli.lewebseite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LewebseiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
